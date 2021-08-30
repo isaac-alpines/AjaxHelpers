2f11a80b12c2424ae1d3f3af51a009b814be9dae
Ishak Alpfidan <isaac.alpines@hotmail.com>
2021-08-30 13:42:44 +0300

    Add remove button and remove list data

--- AjaxHelpers/Controllers/HomeController.cs
+++ AjaxHelpers/Controllers/HomeController.cs
@@ -10,14 +10,33 @@ namespace AjaxHelpers.Controllers
     {
         public ActionResult Index()
         {
+            List<string> listData = new List<string>() { "apple", "samsung", "xiaomi", "huawei", "general mobile", "sony" };
+
+            Session["listData"] = listData;
+
             return View();
         }
 
         public PartialViewResult LoadData()
         {
-            List<string> listData = new List<string>() { "apple", "samsung", "xiaomi", "huawei", "general mobile", "sony"};
+            List<string> listData = Session["listData"] as List<string>;
+
+            System.Threading.Thread.Sleep(1000);
 
             return PartialView("_DataListPartialView", listData);
+        }
+
+        public MvcHtmlString RemoveData(int? id)
+        {
+            List<string> listData = Session["listData"] as List<string>;
+
+            listData.Remove(id.ToString());
+
+            Session["listData"] = listData;
+
+            System.Threading.Thread.Sleep(500);
+
+            return MvcHtmlString.Empty;
         }
     }
 }
